56b11666f4eb32bb9a1e3adea517df5bf2d8f464
naol86 <lijnaolkasinet@gmail.com>
2023-08-17 16:55:55 +0300

    brand complete

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import './brand.css';
+import { AiFillGoogleCircle, AiOutlineSlack, AiOutlineDropbox } from 'react-icons/ai'
+import { DiAtlassian } from 'react-icons/di';
+import { FaShopify } from 'react-icons/fa';
 
 const Brand = () => {
     return (
-        <div>Brand</div>
+        <div className='brand'>
+            <div className='brand__size'>
+                <div className='brand__name-google'>
+                    <AiFillGoogleCircle className='google' /> 
+                    <p>Google</p>
+                </div>
+                <div className='brand__name-slack'>
+                    <AiOutlineSlack />
+                    <p>Slack</p>
+                </div>
+                <div className='brand__name-atlassian'>
+                    <DiAtlassian />
+                    <p>Atlassian</p>
+                </div>
+                <div className='brand__name-dropbox'>
+                    <AiOutlineDropbox />
+                    <p>Dropbox</p>
+                </div>
+                <div className='brand__name-shopify'>
+                    <FaShopify />
+                    <p>Shopify</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
